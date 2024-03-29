bf634186e39024ea4848c4c3479796042ee79255
zaldi <akurizaldirv@gmail.com>
2024-03-29 14:57:17 +0700

    learn variables, int, string

--- main.go
+++ main.go
@@ -3,8 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello")
-	fmt.Print("Hello")
-	fmt.Print("Hello")
-	fmt.Print("Hello")
+	// fmt.Println("Hello")
+	// fmt.Print("Hello")
+	// fmt.Print("Hello")
+	// fmt.Print("Hello")
+
+	var text1 string = "Hello World"
+	var text2 = "Halo Dunia"
+	text3 := "Hola, Amigos"
+
+	fmt.Println(text1)
+	fmt.Println(text2)
+	fmt.Println(text3)
+
+	var num1 int = 20000
+	var num2 int8 = 127
+	var num20 int8 = 127
+	var num3 = 19000
+	num4 := 20000000
+
+	fmt.Println(num1 + num3)
+	fmt.Println(num20 + num2)
+	fmt.Println(num4 / 20)
+
 }
